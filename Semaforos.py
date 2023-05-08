5615372e1a3a55110a961ca5da8e2996bc9ee8e7
IgorMoreira1212 <132018874+IgorMoreira1212@users.noreply.github.com>
2023-05-08 18:03:01 +0100

    start

---
+++ Semaforos.py
@@ -0,0 +1,32 @@
+import pygame
+
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+largura=640
+altura=480
+
+tela=pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('Jogo do Semáforo')
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    #pygame.draw.rect(tela, (255, 0, 0), (200, 300, 40, 50))	Vou precisar
+    #pygame.draw.circle(tela,(0, 0, 120), (360, 260), 40)	Vou precisar
+    pygame.draw.line(tela,(255, 255, 255), (10,10), (10,310), 10)	#1ª linha vertical
+    pygame.draw.line(tela,(255, 255, 255), (110,10), (110,310), 10)	#2ª linha vertical
+    pygame.draw.line(tela,(255, 255, 255), (210,10), (210,310), 10)	#3ª linha vertical
+    pygame.draw.line(tela,(255, 255, 255), (310,10), (310,310), 10)	#4ª linha vertical
+    pygame.draw.line(tela,(255, 255, 255), (410,10), (410,310), 10)	#5ª linha vertical
+    pygame.draw.line(tela,(255, 255, 255), (10,10), (410,10), 10)	#1ª linha horizontal
+    pygame.draw.line(tela,(255, 255, 255), (10,110), (410,110), 10)	#2ª linha horizontal
+    pygame.draw.line(tela,(255, 255, 255), (10,210), (410,210), 10)	#3ª linha horizontal
+    pygame.draw.line(tela,(255, 255, 255), (10,310), (410,310), 10)	#4ª linha horizontal
+    pygame.display.update()
+    
+
+    

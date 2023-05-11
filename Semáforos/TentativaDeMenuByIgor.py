cbf0e6762511a922cd55eb0879276b2bdb9af5a2
IgorMoreira12 <132018874+IgorMoreira1212@users.noreply.github.com>
2023-05-11 04:04:22 +0100

    i am trying

--- Semáforos/TentativaDeMenuByIgor.py
+++ Semáforos/TentativaDeMenuByIgor.py
@@ -4,36 +4,46 @@ from sys import exit
 
 pygame.init()
 
-button_color = (0,0,0,0) # transparente
-hover_color = (255,255,255,100) # branco transparente
-click_color = (255,255,255,150) # branco transparente
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-button_surface = pygame.Surface((100, 50))
-button_surface.fill(button_color)
-button_rect = button_surface.get_rect()
-button_rect.center = (300, 200)
-fundo_imagem = pygame.image.load('FundoSemaforos.png')
+pygame.init()
+
 largura=640
 altura=480
 fonte = pygame.font.Font(None, 50)
 tela=pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Jogo do Semáforo')
+
+# Define as posições e dimensões dos botões
+button_width = 200
+button_height = 50
+button1_pos = (largura//2 - button_width//2, 100)
+button2_pos = (largura//2 - button_width//2, 200)
+button3_pos = (largura//2 - button_width//2, 300)
+
+# Cria os textos dos botões
+button1_text = fonte.render("Novo Jogo", True, WHITE)
+button2_text = fonte.render("Continuar Jogo", True, WHITE)
+button3_text = fonte.render("Regras", True, WHITE)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEMOTION:
-            if button_rect.collidepoint(event.pos):
-                button_surface.fill(hover_color)
-            else:
-                button_surface.fill(button_color)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if button_rect.collidepoint(event.pos):
-                button_surface.fill(click_color)
-        if event.type == pygame.MOUSEBUTTONUP:
-            if button_rect.collidepoint(event.pos):
-                button_surface.fill(hover_color)
-
-    tela.blit(fundo_imagem, (0, 0))
-    tela.blit(button_surface, button_rect)
+        tela.fill(BLACK)
+        pygame.draw.rect(tela, BLACK, (button1_pos[0], button1_pos[1], button_width, button_height))
+        pygame.draw.rect(tela, BLACK, (button2_pos[0], button2_pos[1], button_width, button_height))
+        pygame.draw.rect(tela, BLACK, (button3_pos[0], button3_pos[1], button_width, button_height))
+        tela.blit(button1_text, (button1_pos[0]+10, button1_pos[1]+10))
+        tela.blit(button2_text, (button2_pos[0]+10, button2_pos[1]+10))
+        tela.blit(button3_text, (button3_pos[0]+10, button3_pos[1]+10))
+        pygame.display.update()
+        if event.type==pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = pygame.mouse.get_pos()
+            if pygame.Rect(button1_pos, (button_width, button_height)).collidepoint(mouse_pos):
+                print("Botão 1 pressionado")
+            elif pygame.Rect(button2_pos, (button_width, button_height)).collidepoint(mouse_pos):
+                print("Botão 2 pressionado")
+            elif pygame.Rect(button3_pos, (button_width, button_height)).collidepoint(mouse_pos):
+                print("Botão 3 pressionado")
